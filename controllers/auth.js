a757b7bf7cd12b3057d89d888b7838993f3846e3
Martins Ozolins <ozolins.martinsh@gmail.com>
2025-03-30 19:04:55 +0100

    update auth controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,29 +49,30 @@ export async function login(req, res) {
 
     // returns error if invalid detais
     if (!email || !password) {
-      return res.status(401).send("Invalid username or password");
+      return res.status(401).send("Error: Invalid username or password");
     }
 
     // reads users array from db.json
-    const jsonData = await fetchUser();
+    const data = await fetchUser(email);
+    // {user: [{id: 2, email: email@example.com, password: ...}]}
 
-    // retrieves user based on email
-    const user = jsonData.users.find((user) => user.email === email);
-
-    // returns error if invalid detais
-    if (!user) {
-      return res.status(404).send("Email not found");
+    // if no such email, returns { user: [] }
+    if (data.user.length === 0) {
+      return res.status(401).send("Error: Incorrect email or password");
     }
 
     // compares user's password
-    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      data.user[0].password
+    );
 
     if (!isPasswordCorrect) {
-      return res.status(401).send("Incorrect email or password");
+      return res.status(401).send("Error: Incorrect email or password");
     }
 
     // creates a new jwt
-    const token = signToken(user.id);
+    const token = signToken(data.user[0].id);
 
     // sets a cookie with a jwt within response header
     res.cookie("jwt", token, {
@@ -83,6 +84,10 @@ export async function login(req, res) {
     res.status(200).send("Success");
   } catch (err) {
     console.log(err);
-    res.status(500).send("Error Occurred: Try again later!");
+    if (err.dbError) {
+      res.status(401).send("Error Occured: Password or email incorrect!");
+    } else {
+      res.status(500).send("Error Occurred: Try again later!");
+    }
   }
 }
